fd549a8abc73ae0d09273263f99de689fe379e3e
dlandi <dennis@dennislandi.com>
2024-10-02 20:51:59 -0400

    And ChatGpt Explanation for CSS

--- src/ComponentPenWASM/Program.cs
+++ src/ComponentPenWASM/Program.cs
@@ -15,6 +15,8 @@ namespace ComponentPenWASM
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+
+
             builder.Services.AddScoped<DialogService>();
             builder.Services.AddScoped<NotificationService>();
             builder.Services.AddScoped<RebelThemeService>();
